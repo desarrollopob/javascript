abda314f845e7db870b5019c24c881aafd84f874
jaiber <jaiver.navia@gmail.com>
2024-06-01 11:36:20 -0500

    add js file

---
+++ proyecto/9.project_card.js
@@ -0,0 +1,22 @@
+
+function crearCards() {
+    const numeroCards = document.getElementById('cardCount').value;
+    const cardsContainer = document.getElementById('cardsContainer')
+    //SOLUCIÓN 1
+    cardsContainer.innerHTML = '';
+
+
+    //SOLUCIÓN 2
+  /*   while (cardsContainer.firstChild) {
+        cardsContainer.removeChild(cardsContainer.firstChild)
+    }
+ */
+    for (let i = 0; i < numeroCards; i++) {
+     const card = document.createElement('div');    
+     card.className = 'card';
+     card.innerHTML = `<h2>Card</h2>`
+     cardsContainer.appendChild(card)
+        
+    }
+ 
+}
