bf918a0deb3f3542be60cbfd0b5f4f964109020e
jaiber <jaiver.navia@gmail.com>
2024-06-01 09:54:11 -0500

    add files project

---
+++ 10.for.js
@@ -0,0 +1,13 @@
+
+let datos = [1, 2, 3, 4, 5, 6];
+
+for (let i = 0; i < datos.length;i++ ) {
+
+    if (i.length == 7) {
+        alert('el array es igual a 7')
+    }else{
+        alert('el array es menor a 7')
+    }
+    console.log(i);
+    
+}
