3a5da5f958bc09e1a07eee2e2ab2a087fdd0a0a1
jaiber <jaiver.navia@gmail.com>
2024-09-07 10:02:53 -0500

    new project

---
+++ PETICIONES/script.js
@@ -0,0 +1,18 @@
+document.getElementById("fetchData").addEventListener("click", () => {
+  // URL de la API, por ejemplo, la API de usuarios aleatorios
+  const apiUrl = "https://randomuser.me/api/";
+
+ 
+});
+
+
+
+
+
+
+
+
+
+
+
+
